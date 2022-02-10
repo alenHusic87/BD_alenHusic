1ac02a7b60500e1a5cfaf4a222955c29e0c5cec8
alenHusic87 <92122155+alenHusic87@users.noreply.github.com>
2022-02-10 11:43:24 -0500

    Add files via upload

---
+++ Project_1v4/Project_1v4/Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_1v4
+{
+    class Animal
+    {
+        public void TraitmentAnimal()
+        {
+
+            _MySql _mysql = new _MySql();
+            _mysql.inserToBD();
+
+
+        }
+    }
+}
